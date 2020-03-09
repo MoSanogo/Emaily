87fc65db1e8e6d4d2a79ea7805aa90802ba8c455
Modibo <modisalhydro@gmail.com>
2020-03-09 17:37:13 -0400

    server

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
+require('./services/passport');
 const app = express();
-
-app.get('/', (req, res) => {
-	res.send({ hi: 'there!' });
-});
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
