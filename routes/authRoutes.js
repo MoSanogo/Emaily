79075556a77d688190fc60d9db95cdfd96cf5a93
Modibo <modisalhydro@gmail.com>
2020-03-15 15:38:27 -0400

    44550

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,10 @@
 const passport = require('passport');
 module.exports = (app) => {
 	app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-	// app.get('/auth/google/callback', passport.authenticate('google'), (req, res, next) => {
-	// 	res.redirect('/surveys');
-	// });
 	app.get('/auth/google/callback', passport.authenticate('google'), (req, res, next) => {
-		res.redirect('/api/current_user');
+		res.redirect('/surveys');
 	});
+
 	app.get('/api/logout', (req, res, next) => {
 		req.logout();
 		res.redirect('/');
